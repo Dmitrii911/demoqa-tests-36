0b12cf78f3db603c7f1de208c613f91fa4b55e19
Dmitrii Oleinik <wedil@mail.ru>
2025-06-15 23:51:29 +0500

    Rename test

--- src/test/java/tests/InputTests.java
+++ src/test/java/tests/InputTests.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SearchTests {
+public class InputTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
