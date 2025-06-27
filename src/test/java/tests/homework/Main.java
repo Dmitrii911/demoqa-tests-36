8f9ce70f17ada4209586df8c00138062d3969236
Dmitrii Oleinik <wedil@mail.ru>
2025-06-27 15:48:33 +0500

    add SearchSampleCode

--- src/test/java/tests/homework/Main.java
+++ src/test/java/tests/homework/Main.java
@@ -1,59 +1,53 @@
 package tests.homework;
 
-import org.junit.jupiter.api.Test;
+public class Main {
+    public static void main(String[] args) {
 
-public class Lesson10Test {
     int a = 25;
     int b = 15;
     int c = 10;
     double d = 2.8;
-    void basicsJava0() {
 
+    //Простые арифметические операции
         System.out.println("Сумма = " + (a + b));
         System.out.println("Разница = " + (a - b));
         System.out.println("Произведение = " + a * b);
         System.out.println("Целое частное = " +  a / b + "; остаток = "+ a % b);
-    }
-    @Test
-    void basicsJava1() {
+
+        //Смешанные вычисления
         double result = 0;
-        result = (c+d)/d + c%d - d;
+        result = (c+d)/d + c*d;
 
         System.out.println("Результат = " + result);
-    }
-@Test
-    void basicsJava2() {
+
+        //Логические операции
         if (a>b) {
             System.out.println("1. a действительно больше b");}
             else {
-            System.out.println("1. Утверждение неверное");
-        }
-    if (a<=b) {
-        System.out.println("2. a действительно больше b");}
-    else {
-        System.out.println("2. Утверждение неверное");
-    }
-    if (c==d) {
-        System.out.println("3. Утверждение верное");}
-    else {
-        System.out.println("3. Абра-кадабра");
-    }
-        };
-    @Test
-    void basicsJava3() {
+            System.out.println("1. Утверждение неверное");}
+            if (a<=b) {
+                System.out.println("2. a действительно больше b");}
+            else {
+                System.out.println("2. Утверждение неверное");}
+            if (c==d) {
+                System.out.println("3. Утверждение верное");}
+            else {
+                System.out.println("3. Абра-кадабра");}
+
+        //Работа с предельными значениями примитивных типов
         System.out.println(Float.MAX_VALUE); // Максимальная величина float
         System.out.println(Float.MIN_NORMAL); //Минимальная нормализованная положительная величина
         System.out.println(Float.MIN_VALUE); //Минимальная ненормализованная положительная величина
         System.out.println(Double.MAX_VALUE); // Максимальная величина double
         System.out.println(Double.MIN_NORMAL); //Минимальная нормализованная положительная величина
         System.out.println(Double.MIN_VALUE); //Минимальная ненормализованная положительная величина
-    }
-    @Test
-    void basicsJava4() {
+
+        // Переполнение границ double
         double maxDouble = Double.MAX_VALUE;
-        double result = maxDouble+maxDouble;
-        System.out.println("Результат суммы двух Double.MAX_VALUE = " + result);
+        double result1 = maxDouble+maxDouble;
+        System.out.println("Результат суммы двух Double.MAX_VALUE = " + result1);
         // Infinity бесконечное значение при выходе за границы допустимого диапазона
+    };
     }
-}
+
 
