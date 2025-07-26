7cc5e5658fd0fdc3d738068b48c930fd03116f44
Dmitrii Oleinik <wedil@mail.ru>
2025-07-27 01:23:12 +0500

    add TestData TestDataGen SuitTestData

--- src/test/java/tests/PropertyTestBase.java
+++ src/test/java/tests/PropertyTestBase.java
@@ -15,7 +15,7 @@ public class PropertyTestBase {
     @BeforeAll
     static void basicBrowserSettings() {
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "138.0");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com/");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
